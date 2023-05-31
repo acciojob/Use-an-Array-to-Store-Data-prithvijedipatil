144f5947b29d0207b50ab6ff50875d59ec5ee6a5
prithvijedipatil <prithvipatil09@gmail.com>
2023-05-31 12:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let simpleArray = ['one', 2, 'three', true, false, undefined, null];
 let yourArray=[];
 for(var i=0;i<5;i++){
-	yourArray.push(${simpleArray.[i]});
+	yourArray.push(simpleArray.[i]);
 }
